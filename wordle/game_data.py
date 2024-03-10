4fbc5df52d0d37e9bfc318609fa20e9b6d0c9468
jordan-bott <jordanbott.dev@gmail.com>
2024-03-10 16:07:58 -0700

    adds letter tiles

--- wordle/game_data.py
+++ wordle/game_data.py
@@ -1,1 +1,4 @@
 game_data = {"wordle_index": 1519, "turn_number": 2, "players": ["jordan-bott", "IanKBovard"], "guessed_words": ["PLANT", "HOUSE"], "schema": "<img src=\"./wordle/tiles/grey/P.svg\" width=\"40\" /><img src=\"./wordle/tiles/grey/L.svg\" width=\"40\" /><img src=\"./wordle/tiles/grey/A.svg\" width=\"40\" /><img src=\"./wordle/tiles/grey/N.svg\" width=\"40\" /><img src=\"./wordle/tiles/grey/T.svg\" width=\"40\" /><br/><img src=\"./wordle/tiles/grey/H.svg\" width=\"40\" /><img src=\"./wordle/tiles/grey/O.svg\" width=\"40\" /><img src=\"./wordle/tiles/yellow/U.svg\" width=\"40\" /><img src=\"./wordle/tiles/grey/S.svg\" width=\"40\" /><img src=\"./wordle/tiles/grey/E.svg\" width=\"40\" /><br/>"}
+
+# add letter_schema - store as a list. Index the list for each letter that in changed, using a dictionary to hold what index coresponds to each letter
+# include the breaks in the list at the end of the keyboard rows
