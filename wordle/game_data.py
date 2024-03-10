824d1032d5cca9cf068cdf20545be21f21d91b86
jordan-bott <jordanbott.dev@gmail.com>
2024-03-10 03:09:19 -0700

    wordle index adjustments

--- wordle/game_data.py
+++ wordle/game_data.py
@@ -1,1 +1,1 @@
-game_data = {"wordle_index": null, "turn_number": 0, "players": [], "guessed_words": [], "schema": ""}
+game_data = {"wordle_index": "", "turn_number": 0, "players": [], "guessed_words": [], "schema": ""}
