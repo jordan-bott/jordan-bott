184b4b6821e074a40c685e5b5e2749f031228dff
jordan-bott <jordanbott.dev@gmail.com>
2024-03-09 23:04:47 -0800

    green images

--- my_script.py
+++ my_script.py
@@ -1,3 +1,5 @@
+from "wordle/update_readme.py" import readme_content
+
 file = open("README.md", "a")
 file.write("Added to the readme")
 file.close()
--- wordle/update_readme.py
+++ wordle/update_readme.py
@@ -1,20 +1,4 @@
-Hi! 👋🏼 I'm Jordan, welcome to my github!
-
-📨 jordanbott.dev@gmail.com <br/>
-🗓️ [Meet with me!](https://calendly.com/jordanbott-dev/30min?back=1&month=2024-02) <br/>
-📝 Check out my <a href="./Jordan%20Bott%20Resume.pdf" target="_blank">resume</a>! <br/>
-
-
-<!--START_SECTION:waka-->
-
-```txt
-Markdown     5 mins          ██████████████▓░░░░░░░░░░   58.69 %
-YAML         2 mins          ██████▒░░░░░░░░░░░░░░░░░░   25.59 %
-JavaScript   1 min           ████░░░░░░░░░░░░░░░░░░░░░   15.72 %
-```
-
-<!--END_SECTION:waka-->
-
+readme_content = """
 Hi! 👋🏼 I'm Jordan, welcome to my github!
 
 📨 jordanbott.dev@gmail.com <br/>
@@ -36,6 +20,10 @@ Let's play wordle! Everyone is welcomed to take a turn.
 
 Click "make a guess" below to contribute:
 
-<img src="./wordle/tiles/green/H.svg" />
+<img src="./tiles/green/H.svg" />
 
-![green H](./wordle/tiles/green/H.svg | width="100")
+"""
+
+file = open("README.md", "a")
+file.write(readme_content)
+file.close()
